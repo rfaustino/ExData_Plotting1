1fddfb9cf9bf9143074d81f677b47bdd7d20e946
Rodrigo Faustino <rodrigo_faustino@hotmail.com>
2014-06-09 00:17:03 +0100

    Fourth commit

--- plot3.R
+++ plot3.R
@@ -49,7 +49,7 @@
     points (data$Time, data$Sub_metering_2, type = "l", col = "red")
     points (data$Time, data$Sub_metering_3, type = "l", col = "blue")
 
-    legend ("topright", lwd = 2, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), text.col = c("black", "red", "blue"))
+    legend ("topright", lwd = 2, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
     
     dev.off()
 
--- plot4.R
+++ plot4.R
@@ -44,7 +44,7 @@
     
     
     # Creating the desired plot
-    png(filename="plot3.png", width = 480, height = 480)
+    png(filename="plot4.png", width = 480, height = 480)
     
     par(mfrow = c(2,2))
     
@@ -59,7 +59,7 @@
       plot (data$Time, data$Sub_metering_1, type = "l", col = "black", ylab = "Energy sub metering", xlab = "")
       points (data$Time, data$Sub_metering_2, type = "l", col = "red")
       points (data$Time, data$Sub_metering_3, type = "l", col = "blue")
-      legend ("topright", pch = 45, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), text.col = c("black", "red", "blue"))
+      legend ("topright", pt.cex=0.9, cex=0.9, bty="n", lwd = 2, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
       
       #bottom right plot
       plot(data$Time, data$Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime")
