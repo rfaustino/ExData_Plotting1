a5bc1e01fd74e977d562e1793deb6296fa698840
Rodrigo Faustino <rodrigo_faustino@hotmail.com>
2014-06-08 23:56:50 +0100

    Third commit

--- plot3.R
+++ plot3.R
@@ -48,7 +48,8 @@
     plot (data$Time, data$Sub_metering_1, type = "l", col = "black", ylab = "Energy sub metering", xlab = "")
     points (data$Time, data$Sub_metering_2, type = "l", col = "red")
     points (data$Time, data$Sub_metering_3, type = "l", col = "blue")
-    legend ("topright", pch = 45, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+    legend ("topright", lwd = 2, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), text.col = c("black", "red", "blue"))
     
     dev.off()
 
